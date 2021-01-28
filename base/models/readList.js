2938998547f3551961d167e546e7fabd885b6c17
maddycalvo1 <madison.calvo1@gmail.com>
2021-01-27 21:14:23 -0600

    readList starter code

--- base/models/readList.js
+++ base/models/readList.js
@@ -1,19 +1,19 @@
-var bcrypt = require ("readList");
-const user = require("./user");
+// var bcrypt = require ("readList");
+// const user = require("./user");
 
-module.exports = function(sequelize, DataTypes) {
-    var readList = sequelize.define("Read List", {
-        bookTitle: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isbookTitle: true
-            }
-        },
+// module.exports = function(sequelize, DataTypes) {
+//     var readList = sequelize.define("Read List", {
+//         bookTitle: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true,
+//             validate: {
+//                 isbookTitle: true
+//             }
+//         },
 
-    });
+//     });
 
-}
+// }
 
-// not complete yet
+// // not complete yet
