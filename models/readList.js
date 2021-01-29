6182e79efef88e34010f33c615e4ee75bc95443f
maddycalvo1 <madison.calvo1@gmail.com>
2021-01-28 20:37:21 -0600

    model

--- models/readList.js
+++ models/readList.js
@@ -1,36 +1,35 @@
-// var bcrypt = require("bcryptjs");
-// // Creating our sharedLibrary model
-// module.exports = function(sequelize, DataTypes) {
-//   var ReadList = sequelize.define("List", {
-//       title: {
-//           type: DataTypes.STRING,
-//           allowNull: false,
-//           validate: {
-//               len: [1]
-//           }
-//       },
-//       author: {
-//           type: DataTypes.STRING,
-//           allowNull: false
-//       },
-//       isdn: {
-//           type: DataTypes.STRING,
-//           allowNull: false
-//       },
-//       checkedOut: {
-//           type: DataTypes.BOOLEAN,
-//           defaultValue: false
-//       }
-//     });
-//     // associating the Read List with all users
+var bcrypt = require("bcryptjs");
+module.exports = function(sequelize, DataTypes) {
+  var ReadList = sequelize.define("List", {
+      title: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [1]
+          }
+      },
+      author: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      isdn: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      checkedOut: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: false
+      }
+    });
+  
     
-//     // ReadList.associate = function(models) {
-//     //     ReadList.hasMany(models.User, {
-//     //         foreignKey: {
-//     //             allowNull: false
-//     //         }
-//     //     });
-//     // };
+    // ReadList.associate = function(models) {
+    //     ReadList.hasMany(models.User, {
+    //         foreignKey: {
+    //             allowNull: false
+    //         }
+    //     });
+    // };
     
-//     return ReadList;
-// };
+    return ReadList;
+};
