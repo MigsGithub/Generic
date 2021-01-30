52a7458f58f955ad1797dbeddcd4bf2df864f8d2
Mario Hernandez <mariohernandezk10@gmail.com>
2021-01-30 11:32:00 -0600

    add jquery to grab search value

--- public/js/members.js
+++ public/js/members.js
@@ -1,10 +1,107 @@
-$(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
-  });
-  $.get("https://www.googleapis.com/books/v1/volumes?q=search+terms").then(function(data) {
-    console.log(data)
-  });
+// $(document).ready(function() {
+//   // This file just does a GET request to figure out which user is logged in
+//   // and updates the HTML on the page
+//   $.get("/api/user_data").then(function(data) {
+//     $(".member-name").text(data.email);
+//   });
+//   $.get("https://www.googleapis.com/books/v1/volumes?q=search+terms").then(function(data) {
+//     console.log(data)
+//   });
+// });
+
+$("#textarea1").on("submit", function(event) {
+  event.preventDefault();
+
+  var newBook = {
+    title: $("#textarea1").val(),
+  };
+
+  console.log(newBook);
+
+  // Send the POST request.
+  // $.ajax("/api/burgers", {
+  //   type: "POST",
+  //   data: newBurger
+  // }).then(
+  //   function() {
+  //     console.log("created new cat");
+  //     // Reload the page to get the updated list
+  //     location.reload();
+  //   }
+  // );
 });
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+//   $(document).on("click", ".devour", function(event) {
+//     event.preventDefault();
+//     var id = $(this).data("id");
+//     var boolean = $(this).data("neweatenstatus");
+//     console.log(boolean);
+
+//     // this var helps the ajax to read if its true or false
+//     // this is used in line 16 in the ajax
+//     var newEatenStatus = {
+//       eatenKey: boolean
+//     };
+
+//     // Send the PUT request.
+//     $.ajax("/api/burgers/" + id, {
+//       type: "PUT",
+//       data: newEatenStatus
+//     }).then(
+//       function() {
+//         console.log("changed sleep to", boolean);
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+
+//   $(".create-form").on("submit", function(event) {
+//     // Make sure to preventDefault on a submit event. AKA no reload
+//     event.preventDefault();
+
+//     var newBurger = {
+//       // this is where the input goes aka the name 
+//       name: $("#ca").val().trim(),
+//       // Idk why the "checked" is there but this checks to 
+//       // see if the value is eatenKey or awake
+//       eatenKey: $("[name=eatenKey]:checked").val().trim()
+//     };
+
+//     // Send the POST request.
+//     $.ajax("/api/burgers", {
+//       type: "POST",
+//       data: newBurger
+//     }).then(
+//       function() {
+//         console.log("created new cat");
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   });
+
+//   $(".delete-cat").on("click", function(event) {
+//     let id = $(this).data("id");
+
+//     $.ajax("/api/burgers/" + id, {
+//       type: "DELETE"
+//     }).then(
+//       function() {
+//         // Reload the page to get the updated list
+//         location.reload();
+//       }
+//     );
+//   })
+// });
