581a8d6974e26ab57f223a5f14c22c5f50adf6a8
Mario Hernandez <mariohernandezk10@gmail.com>
2021-02-13 17:23:59 -0600

    fixed this bug

--- models/books.js
+++ models/books.js
@@ -13,7 +13,7 @@ module.exports = function(sequelize, DataTypes) {
           type: DataTypes.STRING,
           allowNull: false
       },
-      isdn: {
+      isbn: {
           type: DataTypes.STRING,
           allowNull: false
       },
